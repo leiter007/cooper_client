698eead0376fce79c0b42bf2673f3aef41aa7b63
raptorf1 <raptor_f1@hotmail.com>
2019-05-15 15:06:55 +0200

    Updates App.js to render LoginForm.jsx if the user presses on the Login button. Gives an id to the actual form in the relevant component. 11 steps of acceptance test green.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
 import React, { Component } from 'react';
 import DisplayCooperResult from './Components/DisplayCooperResult';
 import InputFields from './Components/InputFields';
+import LoginForm from './Components/LoginForm'
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       distance: '',
       gender: 'female',
-      age: ''
+      age: '',
+      renderLoginForm: false
     }
   }
 
-  onChange (event) {
+  onChange(event) {
     this.setState({
       [event.target.id]: event.target.value
-    })
+    });
   }
 
   render() {
+    let renderLogin;
+
+    if (this.state.renderLoginForm === true) {
+      renderLogin = (
+        <LoginForm />
+      )
+    } else {
+      renderLogin = (
+        <button id="login" onClick={() => this.setState({ renderLoginForm: true })}>Login</button>
+      )
+    }
+
     return (
       <>
 
         <InputFields
-        inputChangeHandler={this.onChange.bind(this)}
+          inputChangeHandler={this.onChange.bind(this)}
         />
-
-        <button id="login">Login</button>
 
         <DisplayCooperResult
           distance={this.state.distance}
           gender={this.state.gender}
           age={this.state.age}
         />
+
+        {renderLogin}
 
       </>
     );
