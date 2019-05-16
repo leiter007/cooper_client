84cd1e5050dd2fb1c75301edcbc0baac50c57a40
felix_bonnier <felix_bonnier@yahoo.com>
2019-05-16 13:12:44 +0200

    adds button for showing performance data in App.js. Adds state for renderIndex in constructor function.. Test failing due to not finding text for displaying the results, as expected

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ class App extends Component {
       email: '',
       password: '',
       message: '',
-      entrySaved: false
+      entrySaved: false,
+      renderIndex: false
     }
   }
 
@@ -44,11 +45,15 @@ class App extends Component {
   render() {
     let renderLogin;
     let user;
+    let performanceDataIndex;
 
     if (this.state.authenticated === true) {
       user = JSON.parse(sessionStorage.getItem('credentials')).uid;
       renderLogin = (
         <p>Hi {user}</p>
+      )
+      performanceDataIndex = (
+        <button id="show-index" onClick={() => this.setState({ renderIndex: true })}>Show past entries</button>
       )
     } else {
       if (this.state.renderLoginForm === true) {
@@ -85,8 +90,8 @@ class App extends Component {
           entrySaved={this.state.entrySaved}
           entryHandler={this.entryHandler.bind(this)}
         />
-
         {renderLogin}
+        {performanceDataIndex}
 
       </>
     );
