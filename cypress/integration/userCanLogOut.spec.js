5a3263622ffe0abdbbf9bdb9af48b76a8d7df696
felix_bonnier <felix_bonnier@yahoo.com>
2019-05-19 18:53:39 +0200

    adds feature test for user can log out sucesfully

---
+++ cypress/integration/userCanLogOut.spec.js
@@ -0,0 +1,14 @@
+describe('User can log out', () => {
+  it('succesfully', () => {
+    cy.visit('http://localhost:3001');
+    cy.get('#login').click();
+    cy.get('#login-form').within(() => {
+      cy.get('#email').type('user@mail.com')
+      cy.get('#password').type('password')
+      cy.get('button').click()
+    })
+    cy.contains('Hi user@mail.com')
+    cy.get('#logout').click();
+    cy.contains('Signed out succesfully')
+  })
+})
